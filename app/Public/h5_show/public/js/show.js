b2d54d7897da6f2bd444ff76439c5daab3015fdf
Administrator <1254870992@qq.com>
2016-12-15 17:52:00 +0800

    2016-12-15 h5_show

--- app/Public/h5_show/public/js/show.js
+++ app/Public/h5_show/public/js/show.js
@@ -2,18 +2,80 @@
  * Created by Administrator on 2016/12/14 0014.
  */
 $(function () {
-  var myElement = document.getElementById('show_container');
-  var ham = new Hammer(myElement);
-  ham.get('swipe').set({
-    direction: Hammer.DIRECTION_ALL
-  });
-  document.addEventListener('touchmove', function (event) {
-    event.preventDefault();
-  }, false);
-  ham.on('swipeup', function (ev) {
-    alert('up');
-  });
-  ham.on('swipedown', function (ev) {
-    alert('down');
+  $('#fullpage').fullpage({
+    afterLoad: function(anchorLink, index){
+      //section 1
+      if(index == 1){
+        $('#section0').find('.p1-content2').delay(50).animate({
+          bottom: '10%',
+          opacity: '1'
+        }, 1200, 'easeOutExpo');
+      }
+      //section 2
+      if(index == 2){
+        $('#section1').find('.p2-content1').delay(50).animate({
+          right: '10.8333%'
+        }, 1200, 'easeOutExpo');
+
+        $('#section1').find('.p2-content2').delay(50).animate({
+          left: '-50%'
+        }, 1200, 'easeOutExpo');
+
+      }
+      //section 3
+      if(index == 3){
+       console.log("3");
+      }
+
+      //section 4
+      if(index == 4){
+        console.log("4");
+      }
+
+      //section 5
+      if(index == 5){
+        console.log("5");
+      }
+
+      //section 6
+      if(index == 6){
+        console.log("6");
+      }
+
+    },
+    onLeave :function(index, nextIndex, direction){
+//section 1
+      if(index == 1){
+        $('#section0').find('.p1-content2').removeAttr('style');
+      }
+      //section 2
+      if(index == 2){
+        $('#section1').find('.p2-content1').removeAttr('style');
+        $('#section1').find('.p2-content2').removeAttr('style');
+
+      }
+      //section 3
+      if(index == 3){
+        console.log("3");
+      }
+
+      //section 4
+      if(index == 4){
+        console.log("4");
+      }
+
+      //section 5
+      if(index == 5){
+        console.log("5");
+      }
+
+      //section 6
+      if(index == 6){
+        console.log("6");
+      }
+    },
+    afterRender: function () {
+
+    }
   });
 });
